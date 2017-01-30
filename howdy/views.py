e6c6d1c8afb0cb5373738ec79276985e95bd9727
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-01-30 14:50:00 +0700

    fix: add default value of name

--- howdy/views.py
+++ howdy/views.py
@@ -22,16 +22,32 @@ def hello(request):
 
     name = request.GET.get('name')
 
-    localHour = datetime.now().time().hour
-    if localHour >= 4 and localHour <= 10:
-        greeting = "Good morning, {0}!".format(name)
-    elif localHour >= 11 and localHour <= 15:
-        greeting = "Good day, {0}!".format(name)
-    elif localHour >= 16 and localHour <= 18:
-        greeting = "Good afternoon, {0}!".format(name)
-    elif localHour >= 19 and localHour <= 22:
-        greeting = "Good evening, {0}!".format(name)
+# get name param
+name = request.GET.get('name')
+    
+    # compose head
+    head='<head><title>Spring Hello</title></head>';
+    
+    # get time
+    hour = datetime.now().time().hour
+    greeting=''
+    if hour >= 4 and hour <= 10:
+        greeting = 'Good morning, {0}!'
+    elif hour >= 11 and hour <= 15:
+        greeting = 'Good day, {0}!'
+    elif hour >= 16 and hour <= 18:
+        greeting = 'Good afternoon, {0}!'
+    elif hour >= 19 and hour <= 22:
+        greeting = 'Good evening, {0}!'
     else:
-        greeting = "Good night, {0}!".format(name)
+        greeting = 'Good night, {0}!'
 
-    return HttpResponse (greeting)
+    print hour
+    
+    # compose body
+    body=('<body>'+greeting+'</body>').format(name if name != None else 'hoo-man')
+    
+    # compose html
+    html='<html>'+head+body+'</html>'
+    
+    return HttpResponse(html)
